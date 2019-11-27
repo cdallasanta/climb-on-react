ef55c8b7638c813005ac44a3f1e6c55e231a5cc6
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-11-27 14:46:16 -0800

    header selected link css

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import "../stylesheets/header.scss";
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
   return(
     <header className="header">
-      <div className='nav-item'>
-        <Link to="/preuse_inspections" className="nav-link">Preuse Inspections</Link>
-      </div>
-      <div className='nav-item'>
-        <Link to="/elements" className="nav-link">My Profile (broken)</Link>
-      </div>
-      <div className='nav-item'>
-        <Link to="/elements" className="nav-link">Logout (broken)</Link>
-      </div>
+      <NavLink to="/preuse_inspections" className="nav-link" activeClassName="selected">Preuse Inspections</NavLink>
+      <NavLink to="/elements" className="nav-link" activeClassName="selected">My Profile (broken)</NavLink>
+      <NavLink to="/elements" className="nav-link" activeClassName="selected">Logout (broken)</NavLink>
+      
       <img src={require(`../images/logo.png`)} alt="logo" />
     </header>
   );
