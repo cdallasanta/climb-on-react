e46e541463125e83d53030333fd8046e4f983943
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-11-29 10:51:28 -0800

    creating login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "./stylesheets/global.scss";
+import axios from 'axios';
+import Home from './containers/home';
+import Login from './containers/users/login';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    }
+  }
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    });
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    });
+  }
+
+  loginStatus = () => {
+    axios.get('/logged_in', {withCredentials: true})
+      .then(resp => {
+        if (resp.data.logged_in){
+          this.handleLogin(resp);
+        } else {
+          this.handleLogout();
+        }
+      })
+      .catch(error => console.log('api errors:', error));
+  }
+
+  componentDidMount() {
+    this.loginStatus();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={}/>
+            <Route exact path='/login' component={}/>
+            <Route exact path='/signup' component={}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/containers/user/login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    }
+  }
+
+};
