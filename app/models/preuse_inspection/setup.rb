3c3c5846cdacf3359605ecbd5e56eaa6921c8129
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-12-04 12:39:11 -0800

    rearranging relationships for Sections

--- app/models/preuse_inspection/setup.rb
+++ app/models/preuse_inspection/setup.rb
@@ -3,18 +3,9 @@ class PreuseInspection::Setup < ApplicationRecord
   belongs_to :preuse_inspection
   has_many :user_setups, class_name: "JoinTable::UserSetups"
   has_many :users, through: :user_setups
-  has_many :comments, as: :commentable
+  has_many :sections, as: :inspection
   has_one :element, through: :preuse_inspection
-  accepts_nested_attributes_for :comments
-
-  after_create :default_to_false
-
-  def default_to_false
-    self.equipment_complete ||= false
-    self.element_complete ||= false
-    self.environment_complete ||= false
-    self.save
-  end
+  accepts_nested_attributes_for :sections
 
   # only allows the creation of a takedown once each step of the setup is completed
   def is_complete?
--- app/models/preuse_inspection/takedown.rb
+++ app/models/preuse_inspection/takedown.rb
@@ -5,18 +5,10 @@ class PreuseInspection::Takedown < ApplicationRecord
   has_many :users, through: :user_takedowns
   has_one :element, through: :preuse_inspection
   has_many :climbs, class_name: "Element::Rope::Climb"
-  has_many :comments, as: :commentable
+  has_many :sections, as: :inspection
   has_many :ropes, through: :preuse_inspection, class_name: "Element::Rope"
+  accepts_nested_attributes_for :sections
   accepts_nested_attributes_for :ropes
-  accepts_nested_attributes_for :comments
   accepts_nested_attributes_for :climbs
 
-  after_create :default_to_false
-
-  def default_to_false
-    self.equipment_complete ||= false
-    self.element_complete ||= false
-    self.environment_complete ||= false
-    self.save
-  end
 end
