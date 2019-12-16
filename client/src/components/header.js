1049cc2ff0a2cc05480b928620ad621bf4ed1fff
cdallasanta <chris.dallasanta@gmail.com>
2019-12-16 13:16:08 -0800

    css for dropdown admin tools menu

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -17,31 +17,62 @@ class Header extends React.Component {
   }
 
   adminTools = () => {
-    return (
-      <div>
-        <NavLink>Dashboard</NavLink>
-        <NavLink>Elements</NavLink>
-        <NavLink>Users</NavLink>
-      </div>
-    )
+    if (this.state.showAdminTools){
+      return (
+        <div>
+
+        </div>
+      )
+    }
+    else
+      return null
   }
 
   render(){
     return(
       <header className="header">
-        <NavLink to="/admin" className="nav-link" activeClassName="selected"
-          onMouseEnter={this.handleHover}
-          onMouseLeave={this.handleLeave}>Admin</NavLink>
-        <NavLink to="/preuse_inspections" className="nav-link" activeClassName="selected">Preuse Inspections</NavLink>
-        <NavLink to="/periodic_inspections" className="nav-link" activeClassName="selected">Periodic Inspections</NavLink>
-        <NavLink to="/elements" className="nav-link" activeClassName="selected">My Profile (broken)</NavLink>
-        <NavLink to="/logout" className="nav-link" activeClassName="selected"
+        <div onMouseEnter={this.handleHover} onMouseLeave={this.handleLeave}>
+          <NavLink to="/admin" className="nav-link" activeClassName="selected">Admin</NavLink>
+          <div className="submenu-container">
+            <ReactCSSTransitionGroup
+              transitionName="slide"
+              transitionEnterTimeout={300}
+              transitionLeaveTimeout={300}
+            >
+              {this.state.showAdminTools && <Submenu />}
+            </ReactCSSTransitionGroup>
+          </div>
+        </div>
+        <div>
+          <NavLink to="/preuse_inspections" className="nav-link" activeClassName="selected">Preuse Inspections</NavLink>
+        </div>
+        <div>
+          <NavLink to="/periodic_inspections" className="nav-link" activeClassName="selected">Periodic Inspections</NavLink>
+        </div>
+        <div>
+          <NavLink to="/elements" className="nav-link" activeClassName="selected">My Profile (broken)</NavLink>
+        </div>
+        <div>
+          <NavLink to="/logout" className="nav-link" activeClassName="selected"
           onClick={this.props.handleLogout} >Logout</NavLink>
+        </div>
         
         <img src={require(`../images/logo.png`)} alt="logo" />
       </header>
     )
   }
 };
+
+class Submenu extends React.Component {
+  render() {
+    return (
+      <div className="sub-links">
+        <NavLink to="/admin/dashboard">Dashboard</NavLink>
+        <NavLink to="/admin/elements">Elements</NavLink>
+        <NavLink to="/admin/users">Users</NavLink>
+      </div>
+    )
+  }
+}
 
 export default withRouter(Header);
